9a290aa74f9bea032548accb055327b8e60ccdb8
Connor Mayer <cjmayer@uci.edu>
2023-10-24 10:09:49 -0700

    TELL processing

--- src/generate_turkish_stimuli.py
+++ src/generate_turkish_stimuli.py
@@ -2,57 +2,90 @@
 import csv
 import pandas as pd
 
-consonants = [
-    'm',
-    'n',
-    'p',
-    't',
-    'k',
-    'b',
-    'd',
-    'g',
-    'ɟ',
-    't͡ʃ',
-    'd͡ʒ',
-    'f',
-    's',
-    'ʃ',
-    'h',
-    'v',
-    'z',
-    'ʒ',
-    'l',
-    'ɫ',
-    'j',
-    'ɾ',
-    'c',
+front_consonants = [
+    'c', # k
+    'ɟ', # g
+    'l'  
 ]
 
-vowels = [
+back_consonants = [
+    'k',
+    'g',
+    'ɫ'  # l
+]
+
+labials = [
+    'p',
+    'b',
+    'f',
+    'v',
+    'm'
+]
+
+voiced_stops = [
+    'ɟ',
+    'g',
+    'b',
+    'd',
+    'd͡ʒ',
+]
+
+consonants = [
+    'n',
+    't',
+    'd',
+    't͡ʃ', # ç
+    'd͡ʒ', # c
+    's',
+    'ʃ', # ş
+    'h',
+    'z',
+    'ʒ', # j
+    'j', # y
+    'ɾ'  # r
+] + labials + front_consonants + back_consonants
+
+front_vowels = [
     'i',
-    'y',
-    'ɯ',
-    'u',
+    'y', # ü
     'e',
-    'œ',
+    'œ', # ö
+]
+
+back_vowels = [
+    'ɯ', # ı
+    'u',
     'a',
     'o'
 ]
+
+vowels = front_vowels + back_vowels
 
 real_words = pd.read_csv('data/real_turkish_words.csv')
 real_words = set(real_words.word)
 results = []
 
+real_word_count = 0
 for c1 in consonants:
     for v1 in vowels:
         for c2 in consonants:
             for v2 in vowels:
                 for c3 in consonants:
-                    word = "{} {} {} {} {}".format(
-                        c1, v1, c2, v2, c3
-                    )
-                    if not word in real_words:
-                        results.append([word])
+                    if not (c1 == 'ɾ' or
+                            c1 in front_consonants and v1 in back_vowels or 
+                            c1 in back_consonants and v1 in front_vowels or
+                            c2 in front_consonants and v2 in back_vowels or
+                            c2 in back_consonants and v2 in front_vowels or
+                            c3 in front_consonants and v2 in back_vowels or
+                            c3 in back_consonants and v2 in front_vowels or
+                            c3 in voiced_stops):
+                        word = "{} {} {} {} {}".format(
+                            c1, v1, c2, v2, c3
+                        )
+                        if not word in real_words:
+                            results.append([word])
+                        else:
+                            real_word_count += 1
 
 with open('data/turkish_stimuli_candidates.csv', 'w') as f:
     writer = csv.writer(f)
--- src/process_tell.R
+++ src/process_tell.R
@@ -3,9 +3,13 @@ library(tidyverse)
 setwd("C:/Users/conno/git_repos/turkish_phonotactics")
 #setwd("E:/git_repos/turkish_phonotactics")
 
+########################################################
+# TASK 1: Get a list of all the suffixed forms in TELL #
+########################################################
 elicit_1 <- read_tsv('telldata/ELICIT.db.txt')
 elicit_2 <- read_tsv('telldata/ELICIT2.db.txt')
 
+# Get two elicitation sets and pivot them into useful format
 words_1 <- elicit_1 %>%
   select(-knows, -uses, -ulcase, -lexeme) %>%
   pivot_longer(everything(), names_to = 'type', values_to = 'word')
@@ -14,8 +18,8 @@ words_2 <-  elicit_2 %>%
   select(-knows, -uses, -ulcase, -lexeme) %>%
   pivot_longer(everything(), names_to = 'type', values_to = 'word')
 
+# Filter out weird characters, inconsistent notation
 words <- rbind(words_1, words_2) %>%
-  select(-type) %>%
   filter(!is.na(word)) %>%
   mutate(word=str_replace_all(word, ":", "")) %>%
   mutate(word=str_replace_all(word, ';', '')) %>%
@@ -44,6 +48,67 @@ words <- rbind(words_1, words_2) %>%
   mutate(word=str_replace_all(word, "h i s a ɾ d͡ʒ �", "h i s a ɾ d͡ʒ ɯ")) %>%
   filter(!str_detect(word, 'x')) %>%
   distinct() %>%
+  mutate(word=str_trim(word))
+
+# Check that all the characters in the data are what we expect
+paste0(unique(unlist(strsplit(words$word, ''))), collapse = " ")
+
+# Save output to file
+words %>%
+  select(-type) %>%
   write_csv('data/real_turkish_words.csv')
 
-paste0(unique(unlist(strsplit(words$word, ''))), collapse = " ")
+#################################################################
+# TASK 2: Check for height/back/round harmony in suffixed forms #
+#################################################################
+
+high_vowels <- "[iyɯu]"
+low_vowels <- "[eøao]"
+
+front_vowels <- "[iyeø]"
+back_vowels <- "[ɯuao]"
+
+round_vowels <- "[yuøo]"
+unround_vowels <- "[iɯea]"
+
+# First check in suffixed forms
+vowel_only_suffixed <- words %>%
+  mutate(vowels = str_trim(str_replace_all(word, "[^iyeœɯuao]", ''))) %>%
+  mutate(vowels = map_chr(
+    str_split(vowels, pattern = ""),
+    str_flatten, collapse=" "
+  )) %>%
+  mutate(vowels=str_replace_all(vowels, 'œ', 'ø')) %>%
+  mutate(height_harmony=!(str_detect(vowels, high_vowels) & str_detect(vowels, low_vowels)),
+         back_harmony=!(str_detect(vowels, back_vowels) & str_detect(vowels, front_vowels)),
+         round_harmony=!(str_detect(vowels, round_vowels) & str_detect(vowels, unround_vowels)))
+
+vowel_props_suffixed <- vowel_only_suffixed %>%
+  summarize(prop_height_harmony = mean(height_harmony),
+            prob_back_harmony = mean(back_harmony),
+            prop_round_harmony = mean(round_harmony))
+
+# Now check in citation forms only
+# TODO: This is a coarse way of removing the infinitival suffix,
+# probably captures some nouns too
+vowel_only_citation <- words %>%
+  filter(type == 'citation' & !is.na(word)) %>%
+  mutate(word = ifelse(str_sub(word, -5, -1) %in% c('m a k', 'm e k'),
+                       str_sub(word, 1, -6),
+                       word)) %>%
+  mutate(vowels = str_trim(str_replace_all(word, "[^iyeœɯuao]", ''))) %>%
+  mutate(vowels = map_chr(
+    str_split(vowels, pattern = ""),
+    str_flatten, collapse=" "
+  )) %>%
+  mutate(vowels=str_replace_all(vowels, 'œ', 'ø')) %>%
+  mutate(height_harmony=!(str_detect(vowels, high_vowels) & str_detect(vowels, low_vowels)),
+         back_harmony=!(str_detect(vowels, back_vowels) & str_detect(vowels, front_vowels)),
+         round_harmony=!(str_detect(vowels, round_vowels) & str_detect(vowels, unround_vowels)))
+
+vowel_props_citation <- vowel_only_citation %>%
+  summarize(prop_height_harmony = mean(height_harmony),
+            prob_back_harmony = mean(back_harmony),
+            prop_round_harmony = mean(round_harmony))
+
+# Now check in roots
